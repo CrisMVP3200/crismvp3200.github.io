26a94579b7302484266e1039380bea9475e54ef0
DESKTOP-DHGVAVT\Cristian <cristiancajiaos@gmail.com>
2017-08-22 16:57:46 -0300

    [Open/Codelco/Bootstrap3] Cambios menores

--- open/codelco/demos/custombootstrap3/assets/js/start.js
+++ open/codelco/demos/custombootstrap3/assets/js/start.js
@@ -1,10 +1,7 @@
 var wow = new WOW(
   {
-    animateClass: 'animated', // animation css class (default is animated)
-    offset:       0,          // distance to the element when triggering the animation (default is 0)
-    callback:     function(box) {
-      console.log("Animando...")
-    },
+    animateClass: 'animated', 
+    offset: 0 
   }
 );
 
