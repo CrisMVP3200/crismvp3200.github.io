505a5c70f9b75c593f267f752aeb4d88b970cf28
DESKTOP-DHGVAVT\Cristian <cristiancajiaos@gmail.com>
2017-08-29 12:38:45 -0300

    [Open/Bootstrap4] Se unificó el código de la animación y del scroll en el mismo script. Cambios menores.

--- open/codelco/demos/bootstrap4/assets/js/animation.js
+++ open/codelco/demos/bootstrap4/assets/js/animation.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
-    $(".headeranim").each(function(){ 
-        $(this).addClass("headerslide"); 
-    }); 
-                         
-    $(window).scroll(function(){
-        $(".slideanim").each(function(){
-            var pos = $(this).offset().top;
-            var winTop = $(window).scrollTop(); 
-            if (pos < winTop + 600) {
-                $(this).addClass("slide"); 
-            }
-        });
-    });
+    
 });
